6be3f55de60e1a0811fa646e28cef8b8c2aeb9bb
Alberto Oliveira <albertooliveira@Air-de-Alberto.home>
2022-01-19 11:29:01 +0000

    Reviews listing (Edit Review button not working 100%)

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import { Container } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
-import "./App.css";
-import "./lib/font-awesome/css/all.min.css";
-import Header from "./components/Header/Header";
-import SimpleBottomNavigation from "./components/MainNav";
-import { Routes, Route } from "react-router-dom";
-import Trending from "./Pages/Trending/Trending";
-import Movies from "./Pages/Movies/Movies";
-import Search from "./Pages/Search/Search";
-import Series from "./Pages/Series/Series";
-import Watched from "./Pages/Watched/Watched";
-import Notes from "./Pages/Notes/Notes";
-import { GlobalProvider } from "./components/Context/GlobalState";
+import { Container } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import './lib/font-awesome/css/all.min.css';
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
+import { Routes, Route } from 'react-router-dom';
+import Trending from './Pages/Trending/Trending';
+import Movies from './Pages/Movies/Movies';
+import Search from './Pages/Search/Search';
+import Series from './Pages/Series/Series';
+import Watched from './Pages/Watched/Watched';
+import Notes from './Pages/Notes/Notes';
+import { GlobalProvider } from './components/Context/GlobalState';
 
 function App() {
   return (
-  <>
-  <GlobalProvider>
-  <BrowserRouter>
-  
-  <Header />
-  <div className="app">
-   <Container>
-   <Routes>
-     <Route path='/' element={<Trending />} exact />
-     <Route path='/movies' element={<Movies />} />
-     <Route path='/series' element={<Series />} />
-     <Route path='/search' element={<Search />} />
-     <Route path='/watched' element={<Watched />} />
-     <Route path='/notes' element={<Notes />} />
-   </Routes>
-   </Container>
-  </div>;
-  <SimpleBottomNavigation />
-  </BrowserRouter>
-  </GlobalProvider>
-  </>
+    <GlobalProvider>
+      <BrowserRouter>
+        <Header />
+        <div className="app">
+          <Container>
+            <Routes>
+              <Route path="/" element={<Trending />} exact />
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/series" element={<Series />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/watched" element={<Watched />} />
+              <Route path="/notes" element={<Notes />} />
+            </Routes>
+          </Container>
+        </div>
+        <SimpleBottomNavigation />
+      </BrowserRouter>
+    </GlobalProvider>
   );
 }
 
--- src/Pages/Notes/Notes.js
+++ src/Pages/Notes/Notes.js
@@ -1,131 +1,228 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import "./Notes.css";
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import './Notes.css';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import { Input } from "@material-ui/core";
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import { v4 as uuidv4 } from 'uuid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
+export default function Notes() {
+  const [notes, setNotes] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const LOCAL_STORAGE_KEY = 'MoviesApp.notes';
 
-export default function  Notes () {
+  useEffect(() => {
+    const storedNotes = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log(storedNotes);
+    if (storedNotes) setNotes(storedNotes);
+  }, []);
 
- const {register,handleSubmit,formState: { errors }} = useForm({
-    criteriaMode: "all"
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(notes));
+  }, [notes]);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    criteriaMode: 'all',
   });
 
- const onSubmit = (data) => {
-     console.log(data);
- }
+  const onSubmit = (data) => {
+    console.log(data.id);
+    if (edit) {
+      alert(data.id);
+      console.log(notes);
+      const updated = notes.map((note) => {
+        if (note.id === data.id) {
+          return data;
+        }
+        return note;
+      });
+      setEdit(false);
+      setNotes([...notes, updated]);
+      console.log(updated);
+    } else {
+      setNotes([...notes, data]);
+    }
 
- const [gender, setGender] = React.useState("Male");//
- const handleChange = event => {
+    reset();
+  };
+
+  function clearNotes(review) {
+    const newNotes = notes.filter((note) => note.id !== review);
+    setNotes(newNotes);
+  }
+
+  function editNotes(review) {
+    console.log(review);
+    const editNotes = notes.find((note) => note.id === review);
+
+    setValue('name', editNotes.name);
+    setValue('gender', editNotes.gender);
+    setValue('review', editNotes.review);
+    setEdit(true);
+  }
+
+  const [gender, setGender] = React.useState('Male'); //
+  const handleChange = (event) => {
     setGender(event.target.value);
   };
 
-//  const styles = {
-//     control: base => ({
-//       ...base,
-//       "&:hover": {
-//         borderColor: "red"
-//       }}),
-//       option: (styles, state) => ({
-//         ...styles,
-//         //fontWeight: state.isSelected ? "bold" : "normal",
-//           color: "black"
-//        // backgroundColor: state.isSelected ? 'red' : 'black',
-//       })
-    
-//   };
-
-
-
-
-
-
-  return (  
-
+  return (
     <Grid
-  container spacing={5}
-  position= "relative"
-  overflow= "auto"
-  component="form"
-  onSubmit={handleSubmit(onSubmit)}
-  direction="row"
-  justifyContent="space-around"
-  alignItems="center"
->
+      container
+      spacing={5}
+      position="relative"
+      overflow="auto"
+      component="form"
+      onSubmit={handleSubmit(onSubmit)}
+      direction="row"
+      justifyContent="space-around"
+      alignItems="center"
+    >
+      <Grid item xs={12} md={6}>
+        <InputLabel style={{ color: 'white', paddingTop: '10px' }}>
+          Name
+        </InputLabel>
 
-    
-    
-<Grid item md={6} style={{paddingBottom: "10px"}}>
-      <InputLabel style={{color: "white"}}>Name</InputLabel>
+        <TextField
+          fullWidth
+          id="fullWidth"
+          style={{ backgroundColor: 'white' }}
+          {...register('name', { required: 'Name field is required' })}
+          variant="filled"
+        />
+        <TextField
+          value={uuidv4()}
+          style={{ display: 'none' }}
+          {...register('id')}
+        />
 
-      <TextField fullWidth id="fullWidth" style={{backgroundColor: "white"}} {...register('name', { required: "Name field is required" })} variant="filled" />
-      {errors?.name && <p> {errors?.name.message}</p>}
-      </Grid>     
- 
-<Grid item md={6} style={{paddingBottom: "10px"}}>  
-      <InputLabel style={{color: "white"}}>Gender</InputLabel>
-      <Select style={{backgroundColor:"white", width:"100%" }} {...register("gender")} value={gender} onChange={handleChange}>
-     <MenuItem value={"Male"}>Male</MenuItem>
-     <MenuItem value={"Female"}>Female</MenuItem>
-   </Select>
-   </Grid>  
-  
-   <Grid item md={6}> 
-   <InputLabel style={{color: "white"}}>Review</InputLabel>
-    
-   <textarea  placeholder="Type your review:" style={{width: "100%", height: "150px", paddingBottom: "10px"}} name="review" {...register("review", {required: "Review field is required", minLength: {value:10, message:"Review field must have more than 10 characters"}, maxLength: {value:300,message:"Review field must have less than 300 characters"} })} />
-       {errors?.review && <span>{errors?.review.message}</span>}
-       </Grid> 
+        {errors?.name && <p> {errors?.name.message}</p>}
 
-       <Grid item md={6}> 
-   
-   </Grid>   
-   <Grid item md={12}> 
-   
-   </Grid>
-   
-       
-   <Button variant="contained" type="submit">Submit</Button>
+        <InputLabel style={{ color: 'white', paddingTop: '10px' }}>
+          Gender
+        </InputLabel>
 
-    
+        <Select
+          style={{ backgroundColor: 'white', width: '100%' }}
+          {...register('gender')}
+          value={gender}
+          onChange={handleChange}
+        >
+          <MenuItem value={'Male'}>Male</MenuItem>
+          <MenuItem value={'Female'}>Female</MenuItem>
+        </Select>
+
+        <InputLabel style={{ color: 'white', paddingTop: '10px' }}>
+          Review
+        </InputLabel>
+
+        <TextField
+          multiline
+          rows={4}
+          placeholder="Type your review:"
+          style={{
+            width: '100%',
+            backgroundColor: 'white',
+          }}
+          name="review"
+          {...register('review', {
+            required: 'Review field is required',
+            minLength: {
+              value: 10,
+              message: 'Review field must have more than 10 characters',
+            },
+            maxLength: {
+              value: 300,
+              message: 'Review field must have less than 300 characters',
+            },
+          })}
+        />
+
+        {errors?.review && <span>{errors?.review.message}</span>}
+      </Grid>
+
+      <Grid item xs={12} md={6} style={{ paddingBottom: '10px' }}>
+        <TableContainer>
+          <Table
+            id="split_table"
+            size="small"
+            style={{ backgroundColor: 'white' }}
+          >
+            <TableHead></TableHead>
+            <TableBody>
+              {notes.map((note, index) => (
+                <>
+                  <TableRow key={index}>
+                    <TableCell style={{ borderBottom: 'none' }}>
+                      Name: {note.name}
+                    </TableCell>
+                    <TableCell style={{ borderBottom: 'none' }}>
+                      Gender: {note.gender}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell style={{ borderBottom: 'none' }} colSpan={2}>
+                      Review: {note.review}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell style={{ borderBottom: 'none' }} colSpan={2}>
+                      <Button
+                        variant="contained"
+                        style={{
+                          width: '40%',
+                          marginLeft: '30%',
+                          marginRight: '30%',
+                        }}
+                        onClick={() => clearNotes(note.id)}
+                      >
+                        Clear review
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        style={{
+                          width: '40%',
+                          marginLeft: '30%',
+                          marginRight: '30%',
+                        }}
+                        onClick={() => editNotes(note.id)}
+                      >
+                        Edit review
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                </>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+
+      {/* todos.map((item_todo) => {
+    return <Todo key={item_todo.id} toggleTodo={toggleTodo} todo={item_todo} />;
+  }); */}
+
+      <Grid item md={12}></Grid>
+
+      <Button variant="contained" type="submit">
+        Submit
+      </Button>
     </Grid>
   );
-
-
-//     return (
-//     <form onSubmit={handleSubmit(onSubmit)}>
-        
-//         <InputLabel style={{color: "white"}}>Name</InputLabel>
-//         <TextField id="outlined-basic" label="Name" style={{backgroundColor: "white", marginBottom:"10px"}} {...register('name', { required: "Name field is required" })} variant="outlined" />
-       
-
-//       {errors?.name && <span>{errors?.name.message}</span>}
-     
-//       {/* <Select name="gender" styles={styles} options={options} {...register('gender', { required: "Name field is required" })} /> */}
-      
-//       <InputLabel style={{color: "white"}}>Gender</InputLabel>
-//   <Select style={{backgroundColor:"white", marginBottom:"10px"}} {...register("gender")} value={gender} onChange={handleChange}>
-//     <MenuItem value={"Male"}>Male</MenuItem>
-//     <MenuItem value={"Female"}>Female</MenuItem>
-//   </Select>
-//   <InputLabel style={{color: "white"}}>Review</InputLabel>
-//       <textarea style={{width: "100%", height:"150px"}} placeholder="Type your review:" name="review" {...register("review", {required: "Review field is required", minLength: {value:10, message:"Review field must have more than 10 characters"}, maxLength: {value:300,message:"Review field must have less than 300 characters"} })} />
-//       {errors?.review && <span>{errors?.review.message}</span>}
-
-//       <input type="submit" />
-//     </form>
-//         // <form onSubmit={handleSubmit(onSubmit)}>
-//         //     <input type="text" placeholder="Email" name="email"  {...register('email')}/>
-//         //     <input type="password" placeholder="Password" name="password" {...register('password', { required: true, minLength: 8 })}/>
-//         //     {errors?.password && <span>This field is required</span>}
-//         //     <input type="submit" />
-//         // </form>
-//     );
 }
-
